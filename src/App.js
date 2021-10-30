c4810c095a1e466ab79aa05decdd71fb4263333e
Houssem Marzougui <marzouguih86@gmail.com>
2021-10-29 20:07:49 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+  const [remaining, setRemaining] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, text]);
+    setText('');
+    setRemaining(remaining + 1);
+  }
+
+  const handleClick = (e) => {
+    if (e.currentTarget.className) {
+      e.currentTarget.className = '';
+      setRemaining(remaining + 1);
+    } else {
+      e.currentTarget.className = 'strike';
+      setRemaining(remaining - 1);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form onSubmit={handleSubmit} className="">
+        <input type="text" className="input" value={text} onChange={(e) => setText(e.target.value)} />
+        <button className="btn">Add</button>
+      </form>
+      <p>{remaining} tasks remaining</p>
+      <div className="todos">
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index} onClick={handleClick} data-id={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+
+    </div >
   );
 }
 
